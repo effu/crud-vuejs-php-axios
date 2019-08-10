40a66c638dc41d9d2f1dbcb9f67cdc3b82a38260
effu <cowdell@gmail.com>
2019-08-09 19:29:58 -0600

    Initial load

---
+++ api/contacts.php
@@ -0,0 +1,111 @@
+<?php
+$host = "localhost";
+$user = "vue";
+$password = "_(*&)(*BNKLJN*&^uih5765876%u7^5&65TDyTdYTfyTf";
+$dbname = "vuedb";
+$charset = 'utf8mb4';
+$sql = '';
+$sql_data = [];
+$result = '';
+$stmt = '';
+
+$id = '';
+// $con = mysqli_connect($host, $user, $password, $dbname);
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+// $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+//$input = json_decode(file_get_contents('php://input'),true);
+switch ($method) {
+    case 'GET':
+        $id = isset($_GET['id']) ? intval($_GET['id']) : '';
+        if ($id > 0) {
+            $sql = "select * from contacts where id=:id";
+            $sql_data = ['id' => $id];
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($sql_data);
+            $result = $stmt->fetch();
+
+        } else {
+            $sql = "select * from contacts";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($sql_data);
+            $result = $stmt->fetch();
+
+        }
+
+        break;
+    case 'POST':
+        $id = isset($_POST['id']) ? intval($_POST['id']) : '';
+        $action = isset($_POST['action']) ? $_POST['action'] : '';
+        if ($action == 'delete' && $id > 0) {
+            // delete record with id = $id
+            $sql = "delete from contacts where id = $id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($sql_data);
+        } else {
+            $name = isset($_POST['name']) ? $_POST['name'] : '';
+            $email = isset($_POST['email']) ? $_POST['email'] : '';
+            $city = isset($_POST['city']) ? $_POST['city'] : '';
+            $state = isset($_POST['state']) ? $_POST['state'] : '';
+            $zip = isset($_POST['zip']) ? $_POST['zip'] : '';
+            $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+
+            if ($id > 0) {
+                // change existing record
+                $sql = "update contacts set name = '$name', email = '$email', city='$city', state='$state', zip='$zip', phone='$phone', date_updated = NOW() where id = $id";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute($sql_data);
+                // $result = $stmt->fetch();
+
+            } else {
+                // add new record
+                $sql = "insert into contacts (name, email, city, state, zip, phone, date_created) values ('$name', '$email', '$city', '$state', '$zip', '$phone', NOW())";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute($sql_data);
+                // $result = $stmt->fetch();
+
+            }
+        }
+        break;
+}
+
+// run SQL statement
+// $stmt = $pdo->prepare($sql)->execute($sql_data);
+// $result = $stmt->fetch();
+// $pdo = $stmt->fetchAll();
+// var_dump('<pre>', $result, '</pre>');
+// die if SQL statement failed
+// if (!$stmt || !$result) {
+//     http_response_code(404);
+//     die('Nothing found.');
+// }
+
+if ($method == 'GET') {
+    if (!$id) {
+        // echo '[';
+    }
+
+    echo json_encode($stmt->fetchAll());
+    // for ($i = 0; $i < count($result); $i++) {
+    // echo ($i > 0 ? ',' : '') . json_encode($stmt->fetchAll());
+    // }
+    if (!$id) {
+        // echo ']';
+    }
+} elseif ($method == 'POST') {
+    echo json_encode($stmt->rowCount());
+} else {
+    echo $stmt->$rowCount();
+}
---
+++ js/App.js
@@ -0,0 +1,67 @@
+Vue.component('my-app', {
+  props: { 
+    title: String,
+  },
+  template: `<div class="page-container">
+    <md-app md-mode="reveal">
+      <md-app-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">{{ title }}</span>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <slot>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        </slot>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>`,
+data() {
+  return { menuVisible: false }
+},
+methods: {
+}
+});
