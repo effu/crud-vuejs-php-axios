70d5d5f85ac5b56f5bd7274e931958c172bb6cfa
effu <support@webcarpenter.com>
2019-08-19 09:11:59 -0600

    Added sort functionality

--- js/crud.js
+++ js/crud.js
@@ -1,6 +1,6 @@
     var nums = []
     for (var i = 0; i <= 5000; i++) {
-      nums.push(i)
+        nums.push(i)
     }
 
     var app = new Vue({
@@ -16,9 +16,11 @@
                 id: 0,
                 index: '',
                 rating: 0,
-              },
+            },
             nums: nums,
+            asc: 'desc',
             search: '',
+            orderBy: 'email',
             showDisabled: 0,
             labelDisabled: 'Show Disabled',
             contacts: [],
@@ -30,7 +32,7 @@
             console.log('Vue loading. Getting contacts.')
             let self = this;
             window.addEventListener('keyup', function(event) {
-            // If  ESC key was pressed...
+                // If  ESC key was pressed...
                 if (event.keyCode === 27) {
                     // try close your dialog
                     self.isModalOpen = false;
@@ -39,19 +41,34 @@
             this.getContacts()
         },
         computed: {
-          filteredContacts() {
-            return this.contacts.filter(contact => {
-              return contact.disabled === this.showDisabled &&
-              (this.toLower(contact.name).includes(this.search.toLowerCase()) || 
-              this.toLower(contact.email).includes(this.search.toLowerCase()) ||
-              this.toLower(contact.city).includes(this.search.toLowerCase()) ||
-              this.toLower(contact.state).includes(this.search.toLowerCase()) ||
-              this.toLower(contact.zip).includes(this.search.toLowerCase()) || 
-              this.toLower(contact.phone).includes(this.search.toLowerCase()))
-            })
-          }
+            filteredContacts() {
+                return this.sortContacts(this.orderBy).filter(contact => {
+                    return contact.disabled === this.showDisabled &&
+                        (this.toLower(contact.name).includes(this.search.toLowerCase()) ||
+                            this.toLower(contact.email).includes(this.search.toLowerCase()) ||
+                            this.toLower(contact.city).includes(this.search.toLowerCase()) ||
+                            this.toLower(contact.state).includes(this.search.toLowerCase()) ||
+                            this.toLower(contact.zip).includes(this.search.toLowerCase()) ||
+                            this.toLower(contact.phone).includes(this.search.toLowerCase()))
+                })
+            }
         },
         methods: {
+            sortContacts: function(field) {
+                let sorted = this.contacts.slice().sort(function(a, b) {
+                        return (a[field].toLowerCase() > b[field].toLowerCase()) ? 1 : -1
+                 })
+                 if (this.asc === 'desc') {
+                    return sorted.reverse()
+                 } else {
+                     return sorted
+                 }
+            },
+            setOrderBy: function(field) {
+                this.asc = (this.asc === 'asc') ? 'desc' : 'asc'
+                console.log(this.asc)
+                this.orderBy = field
+            },
             toLower: function(value) {
                 const res = value === null ? '' : value.toLowerCase();
                 return res;
@@ -111,44 +128,44 @@
                         console.log(response)
                         app.filteredContacts.push(contact)
                         app.resetForm()
-                        
+
                     })
                     .catch(function(response) {
                         //handle error
                         console.log(response)
                     })
-                    // this.getContacts()
-                    // this.$forceUpdate()
+                // this.getContacts()
+                // this.$forceUpdate()
             },
             editContact: function(contact, index) {
-                    this.formData = contact;
-                    this.isEditing = true;
-                    this.modalTitle = 'Edit Contact';
-                    this.openModal();
+                this.formData = contact;
+                this.isEditing = true;
+                this.modalTitle = 'Edit Contact';
+                this.openModal();
             },
             deleteContact: function(contact, index) {
-                    if (confirm("Are you sure you want to delete, " + contact.name + "?")){
-                        console.log("Deleting: " + contact.id + " Index: " + index);
-                      let formData = new FormData();
-                      formData.append('id', contact.id)
-                      formData.append('action', 'delete')
-                      axios({
-                        method: 'post',
-                        url: 'api/contacts.php',
-                        data: formData,
-                        config: { headers: { 'Content-Type': 'multipart/form-data' } }
-                    })
-                    .then(function(response) {
-                        console.log(response)
-                        // Vue.delete(app.filteredContacts, index)
-                        contact.disabled = 1;
-                        app.resetForm();
-                    })
-                    .catch(function(response) {
-                        //handle error
-                        console.log(response)
-                    });
-                    }
+                if (confirm("Are you sure you want to delete, " + contact.name + "?")) {
+                    console.log("Deleting: " + contact.id + " Index: " + index);
+                    let formData = new FormData();
+                    formData.append('id', contact.id)
+                    formData.append('action', 'delete')
+                    axios({
+                            method: 'post',
+                            url: 'api/contacts.php',
+                            data: formData,
+                            config: { headers: { 'Content-Type': 'multipart/form-data' } }
+                        })
+                        .then(function(response) {
+                            console.log(response)
+                            // Vue.delete(app.filteredContacts, index)
+                            contact.disabled = 1;
+                            app.resetForm();
+                        })
+                        .catch(function(response) {
+                            //handle error
+                            console.log(response)
+                        });
+                }
             },
             changeContact: function() {
                 console.log("Change contact!")
