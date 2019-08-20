0178a2ee1529662cc50cefd8a2d2363270b122c5
effu <support@webcarpenter.com>
2019-08-20 10:29:15 -0600

    Added copy to clipboard functionality

--- js/crud.js
+++ js/crud.js
@@ -1,3 +1,51 @@
+// Author: Joseph Cowdell
+
+copyText = function(textToCopy) {
+  this.copied = false
+  
+  // Create textarea element
+  const textarea = document.createElement('textarea')
+  
+  // Set the value of the text
+  textarea.value = textToCopy
+  
+  // Make sure we cant change the text of the textarea
+  textarea.setAttribute('readonly', '');
+  
+  // Hide the textarea off the screnn
+  textarea.style.position = 'absolute';
+  textarea.style.left = '-9999px';
+  
+  // Add the textarea to the page
+  document.body.appendChild(textarea);
+
+  // Copy the textarea
+  textarea.select()
+
+  try {
+    var successful = document.execCommand('copy');
+    this.copied = true
+  } catch(err) {
+    this.copied = false
+  }
+
+  textarea.remove()
+}
+
+copyById = function(id) {
+  let text = document.getElementById(id)
+  copyText(text.value)  
+}
+
+copyPreviousSibling = function(curr) {
+  let el = curr.previousElementSibling
+  if (el.value !== undefined) {
+    copyText(el.value)  
+  } else {
+    copyText(el.textContent)
+  }
+}
+
     var nums = []
     for (var i = 0; i <= 5000; i++) {
         nums.push(i)
